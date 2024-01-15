ed1999688bbe863d516d0d64617e42e9e99bddf6
Diego Freniche <diego@freniche.com>
2024-01-15 14:25:08 +0100

    Added search examples

---
+++ examples/MongoDB/AtlasSearch/3FuzzyMatching/3FuzzyMatching.js
@@ -0,0 +1,42 @@
+
+var mongoClient = null;
+var collection
+
+// Connect to MongoDB Atlas
+async function initWebService() {
+  var userName = system.getenv("MONGO_USERNAME")
+  var passWord = system.getenv("MONGO_PASSWORD")
+
+  mongoClient = new MongoClient("mongodb+srv://" + userName + ":" + passWord + "@learn.mongodb.net");
+  collection = mongoClient.getDatabase("search").getCollection("claims")
+}
+
+// search in all fields, but now you can make up to two typos
+// try with: "carr", "policiholder", etc.
+async function get_AtlasSearch(req, res) {
+  var rval = {}
+  
+  var queryTerm = req.query.get("queryTerm")
+
+  rval.searchIndexes = await collection.listSearchIndexes()
+
+  searchOperation = [ 
+    { $search : { 
+      text : { 
+        query: queryTerm , 
+        path:{ wildcard:  '*' },
+        fuzzy: {
+          maxEdits: 2
+        } 
+      } 
+    } 
+    } 
+  ]
+  searchResultsCursor = collection.aggregate(searchOperation)
+
+  rval.searchResult = await searchResultsCursor.toArray()
+
+
+  res.status(201);
+  res.send(rval)
+}
