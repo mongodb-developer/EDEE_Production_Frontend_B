679528c0db09898b774cf0c845dc565b9092f9e4
Diego Freniche <diego@freniche.com>
2024-01-22 17:54:54 +0100

    Move all connect to MongoDB code to the bottom of each file, added comments, removed printing out the index definitions

--- examples/MongoDB/AtlasSearchABNB/7Facets/7Facets.js
+++ examples/MongoDB/AtlasSearchABNB/7Facets/7Facets.js
@@ -2,20 +2,6 @@
 var mongoClient = null;
 var collection
 
-// Connect to MongoDB Atlas
-async function initWebService() {
-  var userName = await system.getenv("MONGO_USERNAME")
-  var passWord = await system.getenv("MONGO_PASSWORD", true)
-
-  if (userName == "" || userName == null || passWord == ""|| passWord == null) {
-    alert("Please enter valid auth");
-    return;
-  }  
-  
-  mongoClient = new MongoClient("mongodb+srv://" + userName + ":" + passWord + "@learn.mongodb.net");
-  collection = mongoClient.getDatabase("sample_airbnb").getCollection("listingsAndReviews");
-}
-
 // to do Facet search we need a new index that supports facets:
 // Here is that pre-created index JSON definition:
 
@@ -42,8 +28,6 @@ async function get_AtlasSearch(req, res) {
   
   var queryTerm = req.query.get("queryTerm")
 
-  rval.searchIndexes = await collection.listSearchIndexes()
-
   searchOperation = [ 
       {
         $searchMeta: {
@@ -64,7 +48,20 @@ async function get_AtlasSearch(req, res) {
 
   rval.searchResult = await searchResultsCursor.toArray()
 
-
   res.status(201);
   res.send(rval)
+}
+
+// Connect to MongoDB Atlas
+async function initWebService() {
+  var userName = await system.getenv("MONGO_USERNAME")
+  var passWord = await system.getenv("MONGO_PASSWORD", true)
+
+  if (userName == "" || userName == null || passWord == ""|| passWord == null) {
+    alert("Please enter valid auth");
+    return;
+  }  
+  
+  mongoClient = new MongoClient("mongodb+srv://" + userName + ":" + passWord + "@learn.mongodb.net");
+  collection = mongoClient.getDatabase("sample_airbnb").getCollection("listingsAndReviews");
 }
